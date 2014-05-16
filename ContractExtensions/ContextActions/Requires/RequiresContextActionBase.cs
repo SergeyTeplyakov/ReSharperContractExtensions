c898c80069d31365a59ca259c09dc47687fc162b
Sergey Teplyakov <sergey.teplyakov@gmail.com>
2014-05-16 11:25:28 +0300

    Added missed files

---
+++ ContractExtensions/ContextActions/Requires/RequiresContextActionBase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using JetBrains.Application.Progress;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.CSharp.Bulbs;
+using JetBrains.ReSharper.Intentions.Extensibility;
+using JetBrains.TextControl;
+using JetBrains.Util;
+
+namespace ReSharper.ContractExtensions.ContextActions.Requires
+{
+    public abstract class RequiresContextActionBase : ContextActionBase
+    {
+        protected readonly ICSharpContextActionDataProvider _provider;
+        protected IUserDataHolder _cache;
+
+        private RequiresAvailability _requiresAvailability = RequiresAvailability.Unavailable;
+        private readonly bool _isGeneric;
+
+
+        protected RequiresContextActionBase(ICSharpContextActionDataProvider provider, bool isGeneric)
+        {
+            Contract.Requires(provider != null);
+
+            _provider = provider;
+            _isGeneric = isGeneric;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant()
+        {
+            Contract.Invariant(_provider != null);
+            Contract.Invariant(_requiresAvailability != null);
+        }
+
+        protected override Action<ITextControl> ExecutePsiTransaction(ISolution solution, IProgressIndicator progress)
+        {
+            Contract.Assert(_requiresAvailability.IsAvailable);
+
+            var executor = new RequiresExecutor(_requiresAvailability, _provider, _isGeneric);
+            executor.ExecuteTransaction(solution, progress);
+
+            return null;
+        }
+
+        public override bool IsAvailable(IUserDataHolder cache)
+        {
+            _requiresAvailability = RequiresAvailability.Create(_provider);
+            _cache = cache;
+
+            return _requiresAvailability.IsAvailable;
+        }
+
+        public override string Text
+        {
+            get
+            {
+                Contract.Assert(_requiresAvailability.IsAvailable);
+                return GetText(_requiresAvailability.SelectedParameterName);
+            }
+        }
+
+        protected abstract string GetText(string selectedParameterName);
+
+    }
+}
---
+++ ContractExtensions/Utilities/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace ReSharper.ContractExtensions.Utilities
+{
+    internal static class StringExtensions
+    {
+        [Pure]
+        public static bool IsNullOrEmpty(this string s)
+        {
+            return string.IsNullOrEmpty(s);
+        }
+    }
+}
