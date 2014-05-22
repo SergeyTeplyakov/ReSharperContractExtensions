6c6a1b52d5eb181e6fe8a958118ad1ae5f37261c
Sergey Teplyakov <sergey.teplyakov@gmail.com>
2014-05-22 08:12:15 +0300

    Added AddContractForContextActionExecuteTest.cs

---
+++ ReSharper.ContractExtensions.Tests/src/ContractFor/AddContractForContextActionExecuteTest.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.ReSharper.Intentions.CSharp.Test;
+using NUnit.Framework;
+using ReSharper.ContractExtensions.ContextActions;
+using ReSharper.ContractExtensions.ContextActions.ContractsFor;
+
+namespace ReSharper.ContractExtensions.Tests.ContractFor
+{
+    [TestFixture]
+    public class AddContractForContextActionExecuteTest : CSharpContextActionExecuteTestBase<AddContractForContextAction>
+    {
+
+        protected override string ExtraPath
+        {
+            get { return "ContractFor"; }
+        }
+
+        [TestCase("ExecutionForInterface")]
+        [TestCase("ExecutionForEmptyInterface")]
+        [TestCase("ExecutionForClass")]
+        [TestCase("ExecutionForEmptyClass")]
+        [Test]
+        public void TestExecution(string testSrc)
+        {
+            DoOneTest(testSrc);
+        }
+    }
+}
