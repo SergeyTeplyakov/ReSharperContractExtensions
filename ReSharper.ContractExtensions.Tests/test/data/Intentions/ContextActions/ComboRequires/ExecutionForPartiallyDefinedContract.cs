4826ba79bb2f7c77136a3321f73dbc6d02513b5b
Sergey Teplyakov <sergey.teplyakov@gmail.com>
2014-05-26 21:16:01 +0300

    Created more complex combo test case.

--- ReSharper.ContractExtensions.Tests/test/data/Intentions/ContextActions/ComboRequires/ExecutionForPartiallyDefinedContract.cs
+++ ReSharper.ContractExtensions.Tests/test/data/Intentions/ContextActions/ComboRequires/ExecutionForPartiallyDefinedContract.cs
@@ -4,6 +4,7 @@ using System.Diagnostics.Contracts;
 abstract class A
 {
   public abstract void EnabledOnAbstractMethod(string s{caret});
+  public abstract void AnotherMethod(string s);
 }
 
 [ContractClassFor(typeof (A))]
