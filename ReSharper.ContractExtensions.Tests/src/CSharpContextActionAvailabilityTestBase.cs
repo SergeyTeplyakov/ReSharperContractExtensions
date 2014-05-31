ce8654355746cb1add43cd8128dfdfbe03b9ff19
Sergey Teplyakov <sergey.teplyakov@gmail.com>
2014-05-31 13:31:50 +0300

    Added support for older R# versions (8.0 and 8.1).

---
+++ ReSharper.ContractExtensions.Tests/src/CSharpContextActionAvailabilityTestBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using JetBrains.Annotations;
+using JetBrains.Application.Components;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.Bulbs;
+using JetBrains.ReSharper.Feature.Services.CSharp.Bulbs;
+using JetBrains.ReSharper.Intentions.Extensibility;
+using JetBrains.TextControl;
+using JetBrains.Util;
+
+namespace JetBrains.ReSharper.Intentions.CSharp.Test
+{
+    public abstract class CSharpContextActionAvailabilityTestBase<TContextAction> : CSharpContextActionAvailabilityTestBase where TContextAction : class, IContextAction
+    {
+        protected override IContextAction CreateContextAction(ICSharpContextActionDataProvider dataProvider)
+        {
+            return (TContextAction)Activator.CreateInstance(typeof (TContextAction), new object[] {dataProvider});
+            //return ShellInstance.GetComponent<TContextAction>();
+        }
+    }
+}
---
+++ ReSharper.ContractExtensions.Tests/src/CSharpContextActionExecuteTestBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using JetBrains.Application.Components;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.CSharp.Bulbs;
+using JetBrains.ReSharper.Intentions.Extensibility;
+using JetBrains.ReSharper.Intentions.Test;
+using JetBrains.TextControl;
+
+namespace JetBrains.ReSharper.Intentions.CSharp.Test
+{
+    public abstract class CSharpContextActionExecuteTestBase<TContextAction> : CSharpContextActionExecuteTestBase where TContextAction : class, IContextAction
+    {
+        protected override IContextAction CreateContextAction(ICSharpContextActionDataProvider dataProvider)
+        {
+            return (TContextAction)Activator.CreateInstance(typeof(TContextAction), new object[] { dataProvider });
+        }
+    }
+}
