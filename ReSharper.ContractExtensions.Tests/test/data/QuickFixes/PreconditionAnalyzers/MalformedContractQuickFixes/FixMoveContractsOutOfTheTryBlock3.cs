d3ba15a327001521fcb8ec3b14e58d07f69ca5c1
Sergey Teplyakov <sergey.teplyakov@gmail.com>
2014-07-24 10:53:21 +0300

    #10. Added missed test files.

---
+++ ReSharper.ContractExtensions.Tests/test/data/QuickFixes/PreconditionAnalyzers/MalformedContractQuickFixes/FixMoveContractsOutOfTheTryBlock3.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics.Contracts;
+
+class A
+{
+  public void Foo(string s)
+  {
+    Contract.Requires(s != null);
+    {
+      try
+      {
+        Contract.Requires(false);
+        {caret}Contract.Ensures(false);
+        Contract.EndContractBlock();
+      }
+      finally {}
+    }
+  }
+}
