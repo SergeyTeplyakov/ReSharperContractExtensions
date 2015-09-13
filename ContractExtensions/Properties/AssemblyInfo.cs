50043877cf51fe8f3bf067556dfa969ef557a89f
Matthew Dippel <matthew.dippel@gmail.com>
2015-09-13 11:36:20 -0400

    Removed Plugin Metadata that has been obsoleted in new API

--- ContractExtensions/Properties/AssemblyInfo.cs
+++ ContractExtensions/Properties/AssemblyInfo.cs
@@ -1,6 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using JetBrains.Application.PluginSupport;
 
 [assembly: AssemblyTitle("ReSharper.ContractExtensions")]
 [assembly: AssemblyProduct("ReSharper.ContractExtensions")]
@@ -8,9 +7,5 @@ using JetBrains.Application.PluginSupport;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: PluginTitle("ReSharper Code Contracts")]
-[assembly: PluginDescription("Bring enhanced experience for Design by Contract development with .NET!")]
-[assembly: PluginVendor("Sergey Teplyakov")]
 
 [assembly: InternalsVisibleTo("ReSharper.ContractExtensions.Tests")]
