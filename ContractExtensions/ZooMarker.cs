2d47342dedf31e578d0fd4e2b46defa5414fe289
Sergey Teplyakov <Sergey.Teplyakov@gmail.com>
2015-10-12 23:54:27 -0700

    Minor fixes.

---
+++ ContractExtensions/ZooMarker.cs
@@ -0,0 +1,10 @@
+﻿using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services;
+using JetBrains.ReSharper.Psi.CSharp;
+
+namespace ReSharper.ContractExtensions
+{
+    [ZoneMarker]
+    public class ZoneMarker : IRequire<ICodeEditingZone>, IRequire<ILanguageCSharpZone> { }
+
+}
